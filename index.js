b82d74de2ca25fba37d95e11107b7846c1afae0d
Scott Houser <Scottanthonyhouser@gmail.com>
2022-10-08 16:57:49 -0600

    feat: first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//questionsArray = require('./JsonData/challenges.cjs');
 const webSocketServerPort = 8000;
 const webSocketServer = require('websocket').server;
 const http = require('http');
@@ -33,63 +34,24 @@ const getUniqueID = () => {
     return s4() + s4() + '-' + s4();
 }
 
-const questionsArray = [
-    {
-        question: 'Which videogame sold the most copies ever',
-        category: 'video game',
-        answer: 'Minecraft',
-    },
-    {
-        question: 'Which olympic sport did the Philippines win their only gold medal',
-        category: 'olympic sport',
-        answer: 'weight lifting',
-    },
-    {
-        question: 'Which actor plays himself in the film Zombieland',
-        category: 'Actor',
-        answer: 'Bill Murry',
-    },
-    {
-        question: 'Which land mammal can\'\t jump',
-        category: 'land mammal',
-        answer: 'elephand',
-    },
-    {
-        question: 'which animal has the most powerful bite',
-        category: 'animal',
-        answer: 'hippopotamus',
-    },
-    {
-        question: 'which country is pop star Shakira from',
-        category: 'country',
-        answer: 'Columbia',
-    },
-    {
-        question: 'Which country does gouda cheese come from',
-        category: 'country',
-        answer: 'Netherlands',
-    },
-    {
-        question: 'What year was Nepoleon defeated at Waterloo',
-        category: 'year',
-        answer: '1815',
-    },
-    {
-        question: 'Which US president had the middle name "Millhouse"',
-        category: 'US president',
-        answer: 'Richard Nixon',
-    },
-    {
-        question: 'Which famous rapper says "Smoke weed every day."',
-        category: 'rapper',
-        answer: 'Snoop Dog',
-    },
-    {
-        question: 'What food is the national dish of England',
-        category: 'food',
-        answer: 'Chicken Tika Masala',
-    },
-]
+ 
+
+  let shuffleArray = (array) => {
+    let curId = array.length;
+    // There remain elements to shuffle
+    while (0 !== curId) {
+      // Pick a remaining element
+      let randId = Math.floor(Math.random() * curId);
+      curId -= 1;
+      // Swap it with the current element.
+      let tmp = array[curId];
+      array[curId] = array[randId];
+      array[randId] = tmp;
+    }
+    return array;
+  }
+
+  //console.log(shuffleArray(challengersArray))
 
 const returnThreeQuestions = () => {
     let questionIndexSelected = []
@@ -106,6 +68,14 @@ const returnThreeQuestions = () => {
     ]
 
     return(returnArray)
+}
+
+const asignPlayersContenders = () => {
+    let newChallengerArray = [...shuffleArray(challengersArray)]
+
+
+    let arrayOfNumbersContenderLength = newChallengerArray.map((item,index)=>{return(index)})
+
 }
 
 wsServer.on('request', function (request){
@@ -168,6 +138,7 @@ wsServer.on('request', function (request){
                                 playerName: JSON.parse(message.utf8Data).userName,
                                 isImpostor: false,
                                 isHost: true,
+                                playerChoices: []
                             },
                         ]
                     }
@@ -192,6 +163,7 @@ wsServer.on('request', function (request){
                             playerName: JSON.parse(message.utf8Data).userName,
                             isImpostor: false,
                             isHost: false,
+                            playerChoices: []
                         }
 
                         allGameState[submittedLobbyId].players.push(newPlayer)
@@ -251,11 +223,22 @@ wsServer.on('request', function (request){
                         
                         let newPlayerArray = allGameState[submittedLobbyId].players
                         newPlayerArray[impostorIndex].isImpostor = true
+                        let newChallengerArray = [...shuffleArray(challengersArray)]
+
+                        let arraySectionStart = 0
+                        newPlayerArray.map((player)=>{
+                            let playerChallengers = newChallengerArray.slice(arraySectionStart, arraySectionStart + 5)
+                            player.playerChoices = [...playerChallengers]
+                            arraySectionStart = arraySectionStart + 5
+                            return player
+                        })
+
                         delete allGameState[submittedLobbyId].players
 
                         allGameState[submittedLobbyId].players = newPlayerArray
                         allGameState[submittedLobbyId].impostorId = newPlayerArray[impostorIndex].playerID
                         allGameState[submittedLobbyId].votesForImpostor = []
+                        allGameState[submittedLobbyId].votesForImpostorWithVoter = []
 
                         allGameState[submittedLobbyId].stageOfTheGame = stageOfTheGame
                         allGameState[submittedLobbyId].directionsToPlayersAfterAction = directionsToPlayersAfterAction
@@ -266,6 +249,12 @@ wsServer.on('request', function (request){
 
                         lobbiesAndTheirClients[submittedLobbyId].forEach((clientKey)=>{
                             clients[clientKey].sendUTF(JSON.stringify({ type:'GAME_STARTING' }));
+                        })
+
+                        let returnGameState = allGameState[submittedLobbyId]
+                       
+                        lobbiesAndTheirClients[submittedLobbyId].forEach((clientKey) => {
+                            clients[clientKey].sendUTF(JSON.stringify({ type:'OTHER_PLAYER_ACTION', payload: allGameState[submittedLobbyId] }));
                         })
 
                     }else{
@@ -312,7 +301,7 @@ wsServer.on('request', function (request){
                         }
                         if(numberOfSubmittedAnswers >= numberOfPlayers ){
                             console.log('Increment Stage of the game')
-                            allGameState[submittedLobbyId].stageOfTheGame = allGameState[submittedLobbyId].stageOfTheGame+1
+                            allGameState[submittedLobbyId].stageOfTheGame = allGameState[submittedLobbyId].stageOfTheGame+1//advance state of game
 
                             lobbiesAndTheirClients[submittedLobbyId].forEach((clientKey) => {
                                 clients[clientKey].sendUTF(JSON.stringify({ type:'SET_A_TIMEOUT', payload: {message: ''} }));
@@ -331,6 +320,25 @@ wsServer.on('request', function (request){
                 }
             }
 
+            //restart game
+            if(JSON.parse(message.utf8Data).type === 'restart game'){
+                if(JSON.parse(message.utf8Data).lobbyId){
+                    let submittedLobbyId = JSON.parse(message.utf8Data).lobbyId.toUpperCase();
+                    let playerId = JSON.parse(message.utf8Data).playerId;
+                    let submittedVote = JSON.parse(message.utf8Data).submittedVote;
+                    if(allGameState[submittedLobbyId]){
+                        
+                        let impostorIndex = Math.floor(Math.random() * allGameState[submittedLobbyId].players.length );
+                        
+                        let newPlayerArray = allGameState[submittedLobbyId].players
+                        newPlayerArray[impostorIndex].isImpostor = true
+
+                    }else{
+                        connection.sendUTF(JSON.stringify({ type:'ERROR', payload: 'could not restart' }));
+                    }
+                }
+            }
+
             //Submit Vote For Impostor
             if(JSON.parse(message.utf8Data).type === 'voteForImpostor'){
                 console.log('SUBMIT VOTE')
@@ -339,8 +347,12 @@ wsServer.on('request', function (request){
                     let playerId = JSON.parse(message.utf8Data).playerId;
                     let submittedVote = JSON.parse(message.utf8Data).submittedVote;
                     if(allGameState[submittedLobbyId]){
-                        
+                        //here add impostor vote to correct player
+
+                        //allGameState[submittedLobbyId].questionsAndAnswers[stageOfTheGame - 1].playerAnswers[playerId] = {answer: submittedAnswer, playerName: playerName} 
+
                         allGameState[submittedLobbyId].votesForImpostor.push(submittedVote)
+                        allGameState[submittedLobbyId].votesForImpostorWithVoter.push({playerId: playerId, votedFor:submittedVote})
 
                         connection.sendUTF(JSON.stringify({ type:'STATE_UPDATE', payload: allGameState[submittedLobbyId] }));
 
@@ -358,3 +370,478 @@ wsServer.on('request', function (request){
     });
 
 })
+
+const challengersArray = [
+    'Clifford the Big Red Dog',
+    'Christopher Columbus',
+    'The Pope',
+    'Kim Kardashian',
+    'Logan Paul',
+    'Helen Keller',
+    'Superman',
+    'Taylor Swift',
+    'Joseph Stalin',
+    'Lebron James',
+    'Albert Einstin',
+    'George Washington',
+    'Mr Beast',
+    'Paul Bunion',
+    'Harambe the Gorilla',
+    'Leonardo Decaprio',
+    'Abraham Lincoln',
+    'arnold schwarzenegger',
+    'Jason Vorhees',
+    'Harry Potter',
+    'Micheal Jackson',
+    'Flipper the dolphin',
+    'Tiger Woods',
+    'Jack Sparrow',
+    'Hulk Hogan',
+    'James Bond',
+    'Sasquatch',
+    'Queen Elizabeth',
+    'Sherlock Holmes',
+    'Dr Phil',
+    'Dracula',
+    'Gengis Khan',
+    'Elon Musk',
+    'Gordon Ramsey',
+    'Ted Bundy',
+    'Vladimir Putin',
+    'Payton Manning',
+    'Sacajuea',
+    'Justin Beiber',
+    'Jackie chan',
+    'me',
+    'My mom',
+    'My dad',
+    'my grandma',
+    'My grandpa',
+    'Luke Skywalker',
+    'Ghandi',
+    'The insane clown posse',
+    'Dr Frankenstein',
+    'Tony Saprano',
+    'Batman',
+    'Mike Tyson',
+    'Amadaeus Mozart',
+    'William Shakespear',
+    'Neil Armtrong',
+    'Jesus Christ',
+    'Tom Sawyer',
+    'Al Capone',
+    'Kanye West',
+    'Usain Bolt',
+    'Tekashi 69',
+    'Bigfoot',
+    'Air bud',
+    'Billy the kid',
+    'smokey the bear',
+    'the hamburgler',
+    'Billey Eilish',
+    'ebinezier scrouge',
+    'Elmo',
+    'Homer simpson',
+    'kids birthday clown',
+    'Moses',
+    'Stephen king',
+    'Thomas Edison',
+    'E.T',
+    'My significant other',
+    'Shrek',
+    'Paul Blart Mall Cop',
+    'Mufasa',
+    'The Joker',
+    'Zeus',
+    'Hercules',
+    'Mary Poppins',
+]
+
+const questionsArray = [
+    {
+        question: 'Win a rap battle',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Drink the most Vodka',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win a Boxing match',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Be the best therapist',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Slay a vampire',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'win a presidential race',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best Sumo Wrestler',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best Secret agent',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win a hotdog eating contest',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win a beauty pagent',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win a marathon',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Bake the best cake',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Jump the highest',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win a game of Chess',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'rescue a drowning swimmer',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Assassinate the president',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win go carts at chuck e cheese',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'negotiate peace between israel and Palestine',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'fit the most marshmallows in their mouth',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'win the tour de france',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Be the best therapist',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Slay a vampire',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Run a fortune 500 company',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'win a presidential race',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'strongest arm wrestler',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'most likes on instagram',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Hide from the cops the longest',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best Romantic Partner',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win a game of golf',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best Pro Wrestler',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best role model for children',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'get their idea funded on shark tank',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Be the best Roommate',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best at medieval jousting',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win American Idol',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win a triathlon',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Sell the most girl scout cookies',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best first grade teacher',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'survive in the wilderness the longest',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best broadway performance',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best zombie apocalypse survivor',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best movie villain',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win a talent show',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Win an Esports tournament',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Wildest outlaw',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'best boss',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best standup comedian',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'best astronaut',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Wait on 10 tables at once',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'live the longest',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'most ruthless gangster',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best breakfast cereal mascot',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Powerlifting',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Most matches on tinder',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'best public speaker',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Solve a cold case murder',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'be completely useless',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'read the most books in one day',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'most emotionally manipulative',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best haunted house actor',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Best child birthday party entertainer',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'best highschool teacher',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'worst babysitter',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'be the coolest action figure',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'best at keeping a secret',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'best kisser',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'character you love to hate',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'roll the dankest blunt',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'toss a dwarf the farthest',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'host the best podcast',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'best dunegon master',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'best guardian angel',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Biggest drama queen',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'most likley to be a serial killer',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'Most Twitter followers',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'fight to the death',
+        category: '',
+        answer: '',
+    },
+    {
+        question: 'best prom date',
+        category: '',
+        answer: '',
+    },
+    
+]
